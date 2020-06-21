3f29aa92f944240228b1862117e414e5ab9a1f9f
Konrad Pazgan <hello@konradpazgan.pl>
2020-06-21 11:44:19 +0200

    Created story edit action.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session')
 const mongoose = require('mongoose')
@@ -22,6 +23,16 @@ const app = express();
 //Body parser
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+// Method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
 
 // logging
 if (process.env.NODE_ENV === 'development') {
